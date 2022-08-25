d986e70ccb28df144f8df08494f7bb4b6759e342
Sujon Roy <104683977+Speed-No-1@users.noreply.github.com>
2022-08-25 12:27:02 +0600

    send message with limit or unlimited on fb-msg

---
+++ unlimit_fb_mgs.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+while True:
+    time.sleep(4)
+    pyautogui.typewrite("tui akta beyman..murgi chor..aka aka murgi gulo khaicis.tui akta beyman..murgi chor..aka aka murgi gulo khaicis")
+    time.sleep(2)
+    pyautogui.press('enter')
